44c34dec0bf5607424ba8aceff8634dfd65aafaf
JackSmoot <smooja01@gettysburg.edu>
2025-03-10 14:07:43 -0400

    Added Enemy
    Added enemy that patrols along a preset path.

--- popup/game.js
+++ popup/game.js
@@ -10,13 +10,60 @@
 const player = document.getElementById("player");
 const gameWindow = document.querySelector(".game-window");
 const obstacles = document.querySelectorAll(".obstacle");
-
+const enemies = document.getElementById("enemy");
 
 let posX = 0, posY = 0;
 const step = 20;
 
 const gameWidth = gameWindow.clientWidth - 20;
 const gameHeight = gameWindow.clientHeight - 20;
+
+const patrolPath = [
+    { x: 90, y: 10 }, // top right
+    { x: 90, y: 30 },
+    { x: 90, y: 50 },
+    { x: 90, y: 70 },
+    { x: 90, y: 90 },
+    { x: 90, y: 110 },
+    { x: 90, y: 130 }, // bottom right
+    { x: 70, y: 130 },
+    { x: 50, y: 130 },
+    { x: 30, y: 130 },
+    { x: 10, y: 130 } // bottom left
+];
+
+let enemyIndex = 0;
+let movingForward = true;
+
+enemy.style.left = patrolPath[0].x + "px";
+enemy.style.top = patrolPath[0].y + "px";
+
+function moveEnemy() {
+
+    let target = patrolPath[enemyIndex];
+
+    enemy.style.left = target.x + "px";
+    enemy.style.top = target.y + "px";
+
+    checkLose();
+
+    // Update enemy index for next movement
+    if (movingForward) {
+        enemyIndex++;
+        if (enemyIndex >= patrolPath.length) {
+            movingForward = false;
+            enemyIndex -= 2;
+        }
+    } else {
+        enemyIndex--;
+        if (enemyIndex < 0) {
+            movingForward = true;
+            enemyIndex = 1;
+        }
+    }
+}
+
+setInterval(moveEnemy, 250);
 
 function movePlayer(dx, dy) {
     newX = Math.max(0, Math.min(gameWidth, posX+dx));
@@ -29,6 +76,7 @@ function movePlayer(dx, dy) {
     }
 
     checkWin();
+    checkLose();
 }
 
 function checkWin() {
@@ -43,6 +91,24 @@ function checkWin() {
 
     if (isTouching) {
         alert("You win!");
+        posX = 0;
+        posY = 0;
+        movePlayer(0, 0);
+    }
+}
+
+function checkLose() {
+    const enemyRect = enemy.getBoundingClientRect();
+    const playerRect = player.getBoundingClientRect();
+
+    const isTouching =
+        playerRect.left < enemyRect.right &&
+        playerRect.right > enemyRect.left &&
+        playerRect.top < enemyRect.bottom &&
+        playerRect.bottom > enemyRect.top;
+
+    if (isTouching) {
+        alert("Game Over! The enemy got you!");
         posX = 0;
         posY = 0;
         movePlayer(0, 0);
